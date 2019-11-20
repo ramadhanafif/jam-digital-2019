4a3664e8011bde349526caceac5d048a663e52a6
waskita <waskita@ee.itb.ac.id>
2019-11-20 07:31:37 +0700

    Add files via upload

---
+++ fsm/fsm.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "../fsm/fsm.h"
+
+void fsm_debounce(int *input, int *state, int *variable) {
+
+}
+
+void fsm_jam(int mode_clean, int set_clean, int *state, int *variable) {
+
+}
+
+void fsm_lengkap(int mode_in, int set_in, int *state, int *variable) {
+
+}
+
+/**
+ * initialize all states
+ */
+void fsm_init() {
+
+}
