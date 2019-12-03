696228d6a676bc90a1cb65ee27429f68360906ef
ramadhanafif <31378187+ramadhanafif@users.noreply.github.com>
2019-12-03 09:45:26 +0700

    koreksi overflow h, m, s, d, M

--- fsm/fsm.c
+++ fsm/fsm.c
@@ -1,146 +1,158 @@
-	/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*
+  To change this license header, choose License Headers in Project Properties.
+  To change this template file, choose Tools | Templates
+  and open the template in the editor.
+*/
 
 #include "../fsm/fsm.h"
 
 void fsm_debounce(int input, int *state, int *variable) {
-	// increment variable
-	// increment terjadi setiap 1 ms
-	// input dianggap berhenti bouncing setelah variable bernilai 10
+  // increment variable
+  // increment terjadi setiap 1 ms
+  // input dianggap berhenti bouncing setelah variable bernilai 10
 
-	if (*variable < 10)
-	{
-		*variable++;
-	}
-	else //variable>10
-	{
-		if (input == 1)
-			*state = 1;
-	}
+  if (*variable < 10)
+  {
+    *variable++;
+  }
+  else //variable>10
+  {
+    if (input == 1)
+      *state = 1;
+  }
 }
 
 void fsm_jam(int mode_clean, int set_clean, int *state, int *variable) {
-	int curr_state = *state;
-	int next_state;
-	switch (curr_state)
-	{
-	case S_TIME:
-		if (mode_clean)
-			next_state = S_DATE;
-		
-		if (set_clean)
-			next_state = S_TIME_HOUR;
-		break;
-	
-	case S_DATE:
-		if (mode_clean)
-			next_state = S_TIME;
-		
-		if (set_clean)
-			next_state = S_DATE_HOUR;
-		break; 
-/*--------------------------------------------------------*/
-	case S_TIME_HOUR:
-		if (mode_clean){
-			next_state = S_TIME_HOUR;
-			*variable++;
-		}
-		if (set_clean)
-			next_state = S_TIME_MINUTE;
-		break; 
+  int curr_state = *state;
+  int next_state;
+  switch (curr_state)
+  {
+    case S_TIME:
+      if (mode_clean)
+        next_state = S_DATE;
 
-	case S_TIME_MINUTE:
-		if (mode_clean){
-			next_state = S_TIME_MINUTE;
-			*variable++;
-		}
-		if (set_clean)
-			next_state = S_TIME_DAY;
-		break; 
+      if (set_clean)
+        next_state = S_TIME_HOUR;
+      break;
 
-	case S_TIME_DAY:
-		if (mode_clean){
-			next_state = S_TIME_DAY;
-			*variable++;
-		}
-		if (set_clean)
-			next_state = S_TIME_MONTH;
-		break; 
+    case S_DATE:
+      if (mode_clean)
+        next_state = S_TIME;
 
-	case S_TIME_YEAR:
-		if (mode_clean){
-			next_state = S_TIME_YEAR;
-			*variable++;
-		}
-		if (set_clean)
-			next_state = S_TIME;
-		break; 
-/*--------------------------------------------------------*/
-	case S_DATE_HOUR:
-		if (mode_clean){
-			next_state = S_DATE_HOUR;
-			*variable++;
-		}
-		if (set_clean)
-			next_state = S_DATE_MINUTE;
-		break; 
+      if (set_clean)
+        next_state = S_DATE_HOUR;
+      break;
+    /*--------------------------------------------------------*/
+    case S_TIME_HOUR:
+      if (mode_clean) {
+        next_state = S_TIME_HOUR;
+        *variable++;
+        if (*variable == 24)
+          *variable = 0;
+      }
+      if (set_clean)
+        next_state = S_TIME_MINUTE;
+      break;
 
-	case S_DATE_MINUTE:
-		if (mode_clean){
-			next_state = S_DATE_MINUTE;
-			*variable++;
-		}
-		if (set_clean)
-			next_state = S_DATE_DAY;
-		break; 
+    case S_TIME_MINUTE:
+      if (mode_clean) {
+        next_state = S_TIME_MINUTE;
+        *variable++;
+        if (*variable == 60)
+          *variable = 0;
+      }
+      if (set_clean)
+        next_state = S_TIME_DAY;
+      break;
 
-	case S_DATE_DAY:
-		if (mode_clean){
-			next_state = S_DATE_DAY;
-			*variable++;
-		}
-		if (set_clean)
-			next_state = S_DATE_MONTH;
-		break; 
+    case S_TIME_DAY:
+      if (mode_clean) {
+        next_state = S_TIME_DAY;
+        *variable++;
+        if (*variable == 31)
+          *variable = 0;
+      }
+      if (set_clean)
+        next_state = S_TIME_MONTH;
+      break;
 
-	case S_DATE_YEAR:
-		if (mode_clean){
-			next_state = S_DATE_YEAR;
-			*variable++;
-		}
-		if (set_clean)
-			next_state = S_DATE;
-		break;
+    case S_TIME_YEAR:
+      if (mode_clean) {
+        next_state = S_TIME_YEAR;
+        *variable++;
+      }
+      if (set_clean)
+        next_state = S_TIME;
+      break;
+    /*--------------------------------------------------------*/
+    case S_DATE_HOUR:
+      if (mode_clean) {
+        next_state = S_DATE_HOUR;
+        *variable++;
+        if (*variable == 24)
+          *variable = 0;
+      }
+      if (set_clean)
+        next_state = S_DATE_MINUTE;
+      break;
 
-	//next state assignment
-	*state = next_state
+    case S_DATE_MINUTE:
+      if (mode_clean) {
+        next_state = S_DATE_MINUTE;
+        *variable++;
+        if (*variable == 60)
+          *variable = 0;
+      }
+      if (set_clean)
+        next_state = S_DATE_DAY;
+      break;
 
-}
+    case S_DATE_DAY:
+      if (mode_clean) {
+        next_state = S_DATE_DAY;
+        *variable++;
+        if (*variable == 32)
+          *variable = 0;
+      }
+      if (set_clean)
+        next_state = S_DATE_MONTH;
+      break;
 
-void fsm_lengkap(int mode_in, int set_in, int *state, int *variable) {
-	int mode_counter;
-	int set_counter;
+    case S_DATE_YEAR:
+      if (mode_clean) {
+        next_state = S_DATE_YEAR;
+        *variable++;
+      }
+      if (set_clean)
+        next_state = S_DATE;
+      break;
 
-	int mode_clean;
-	int set_clean;
+      //next state assignment
+      *state = next_state
 
-	fsm_debounce(mode_in,mode_clean,mode_counter);
-	fsm_debounce(set_in,set_clean,mode_counter);
+  }
 
-	fsm_jam(mode_clean,set_clean,state,variable);
-	
-}
+  void fsm_lengkap(int mode_in, int set_in, int *state, int *variable) {
+    int mode_counter;
+    int set_counter;
 
-/**
- * initialize all states
- */
-void fsm_debounce_init(int *state, int *variable) {
-	*variable = 0;
-}
+    int mode_clean;
+    int set_clean;
 
-void fsm_jam_init(int *state, int *variable) {
-    *state = S_TIME_HOUR;
-}
+    fsm_debounce(mode_in, mode_clean, mode_counter);
+    fsm_debounce(set_in, set_clean, mode_counter);
+
+    fsm_jam(mode_clean, set_clean, state, variable);
+
+  }
+
+  /* Debouce counter reset */
+  void fsm_debounce_init(int *state, int *variable) {
+    *variable = 0;
+  }
+
+
+  /* Initial state set */
+  void fsm_jam_init(int *state, int *variable) {
+    *state = S_TIME;
+  }
--- fsm/fsm.h
+++ fsm/fsm.h
@@ -1,15 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+   To change this license header, choose License Headers in Project Properties.
+   To change this template file, choose Tools | Templates
+   and open the template in the editor.
+*/
 
-/* 
- * File:   newfile.h
- * Author: waskita
- *
- * Created on November 20, 2019, 7:06 AM
- */
+/*
+   File:   newfile.h
+   Author: waskita
+
+   Created on November 20, 2019, 7:06 AM
+*/
 
 #ifndef FSM_H
 #define FSM_H
@@ -22,12 +22,12 @@ extern "C" {
 #define S_DATE      1
 
 #define S_YEAR      11
-#define S_MONTH     12  
+#define S_MONTH     12
 #define S_DAY       13
 #define S_HOUR      14
 #define S_MINUTE    15
-  
-#define S_TIME_YEAR     21  
+
+#define S_TIME_YEAR     21
 #define S_TIME_MONTH    22
 #define S_TIME_DAY      23
 #define S_TIME_HOUR     24
@@ -38,6 +38,14 @@ extern "C" {
 #define S_DATE_DAY      33
 #define S_DATE_HOUR     34
 #define S_DATE_MINUTE   35
+
+void fsm_debounce(int input, int *state, int *variable);
+void fsm_jam(int mode_clean, int set_clean, int *state, int *variable);
+void fsm_lengkap(int mode_in, int set_in, int *state, int *variable);
+void fsm_debounce_init(int *state, int *variable);
+void fsm_jam_init(int *state, int *variable);
+
+
 
 #ifdef __cplusplus
 }
